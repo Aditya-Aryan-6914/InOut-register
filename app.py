203d2465f3572205ff4ad52d47a9e04f694af091
SnehaAinapure <snehaainapure082@gmail.com>
2026-07-31 10:30:52 +0000

    Implement basic Flask routes and templates for home and admin pages

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
- 
+def home():
+    return render_template("index.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
